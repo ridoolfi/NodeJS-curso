a30c73519a6ed02d6a098ce8b6e98f80c0ce624d
ridoolfi <angeloggc2@gmail.com>
2023-10-03 02:51:38 -0300

    Execução da Criação de Rotas e Exibindo HTML

--- 7 - Exibir HTML/main.js
+++ 7 - Exibir HTML/main.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express();
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname)
+    res.sendFile(__dirname + "/html/index.html")
 });
 
 app.get("/sobre", function(req, res){
-    res.send("Minha Pagina Sobre")
+    res.sendFile(__dirname + "/html/sobre.html")
 })
 
 app.get("/blog", function(req,res){
